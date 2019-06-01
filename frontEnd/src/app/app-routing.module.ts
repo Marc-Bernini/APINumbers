cca57f15973e114a2d472d571d389379bb2039c2
Marc Bernini <bernini.marc@gmail.com>
2019-06-01 11:37:15 +0200

    Add routes for homePage and page2

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { Page2Component } from './components/page2/page2.component';
 
 const routes: Routes = [
   {
     path: '', component: HomePageComponent
+  },
+  {
+    path: 'page2', component: Page2Component
   }
 ];
 
