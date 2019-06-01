44004404fe17cf22aef3c8428b618197da1b45f7
Marc Bernini <bernini.marc@gmail.com>
2019-06-01 11:43:52 +0200

    Change directory tree, add a components directory

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { Page2Component } from './components/page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavBarComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
