27f3df1e69f16a53f4886e451292c339324ea627
Marc Bernini <bernini.marc@gmail.com>
2019-06-01 17:39:22 +0200

    clean code, remove constructor and OnInit directive where no need it

--- frontEnd/src/app/components/home-page/home-page.component.ts
+++ frontEnd/src/app/components/home-page/home-page.component.ts
@@ -9,16 +9,21 @@ import { GetNumberAPIService } from '../../services/get-number-api.service';
 export class HomePageComponent {
 
   results: Array<object> = []; // List of user's result
+
   date: string; // Number choose by user
 
-  dateChooses: Array<string> = [];
+  dateChooses: Array<string> = []; // List of user's date
+
+  response: boolean; // To disable button during API request
 
   constructor(private getNumber: GetNumberAPIService) { }
 
   onSubmit() {
+    this.response = true; // Disable button
+
     const userDate = this.date.split(','); // Split user's answer to transform it in array
 
-    for (let i = 0; i < userDate.length ; i++) { // Make a loop to get all API response in one submit
+    for (let i = 0 ; i < userDate.length ; i ++) { // Make a loop to get all API response in one submit
 
       this.getNumber.chooseDate(userDate[i]) // Call service's method
       .subscribe(data => { // Subscribe to the API call
@@ -28,9 +33,11 @@ export class HomePageComponent {
         this.results.push(answer); // Push results in result's list
 
         this.getNumber.transfertResult(answer, userDate[i]); // Call service to push result and transfert it to page 2
-        return this.results;
+
+        this.response = false; // Enable button after API call
       });
     }
+
     this.date = ''; // Initialize input after submit
   }
 }
