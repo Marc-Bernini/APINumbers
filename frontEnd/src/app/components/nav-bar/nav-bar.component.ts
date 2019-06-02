7479f5ff6daffc1533e1666c4df5496fc6714f36
Marc Bernini <bernini.marc@gmail.com>
2019-06-02 18:29:22 +0200

    Add ngClass and function to navbar component to know on which page user is on

--- frontEnd/src/app/components/nav-bar/nav-bar.component.ts
+++ frontEnd/src/app/components/nav-bar/nav-bar.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  home = true; // To know on which page user is
+
+  // Function to switch class depending on which page user is on
+  userPage(page) {
+    switch (page) {
+      case 'home':
+        this.home = true;
+        break;
+
+      case 'page2':
+        this.home = false;
+        break;
+    }
+  }
+
 }
